41e1948ac89cc463b49cc85f92372ad0a6d185a8
buzhihuojie <ren.weiluo@gmail.com>
2016-02-03 13:01:41 -0800

    add expXdrift_linearYdrift model

--- dm_expXdrift.py
+++ dm_expXdrift.py
@@ -6,6 +6,7 @@ Created on Jan 28, 2016
 
 from importlib import reload
 import delarueMenozzi_simplifedCase_base
+from run_dm_iteration_helpers import dm_iterate_helper, dm_iterate_then_pickle
 reload(delarueMenozzi_simplifedCase_base)
 
 from delarueMenozzi_simplifedCase_base import DelarueMenozziSimplifiedCaseBase
@@ -80,5 +81,56 @@ class DM_expXdrift_linearYdrift(DM_expXdrift):
         return -2 * self.beta * y
     
     
- 
+class DM_expXdrift_linearYdrift_useExpectationOnYdrift(DM_expXdrift_linearYdrift):
+    '''
+    dQ_t = [Ae^{-\kappa \Delta^b_t} - Ae^{-\kappa \Delta^a_t}] dt + \sigma dB_t
+    dY_t = -2\beta eta_t dt + Z_t dB_t
+    Q_0 = q_0; Y_T = -2m Q_T
+    where \Delta^b_t = 1 / \kappa - Y_t; \Delta^a_t = 1 / \kappa + Y_t
+    '''
+    
+    
+    
+    def __init__(self, eta, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.eta = eta
+        
+    def G(self, time_index, y):
+        return -2 * self.beta * self.eta[time_index]
+    
+    
+def expXdrift_linearYdrift_factory(phase, *args, **kwargs):
+    if phase == 'init':
+        return DM_expXdrift_linearYdrift(*args, **kwargs)
+    elif phase == 'use_expectation':
+        return DM_expXdrift_linearYdrift_useExpectationOnYdrift(*args, **kwargs)
+    else:
+        raise Exception("phase should be " +
+                         "either 'init' for the starting phase " +
+                         "or 'use_expectation' for the following phase")
+def iterate_expXdrift_linearYdrift(iter_number = 20, *args, **kwargs):
+    return dm_iterate_helper(expXdrift_linearYdrift_factory,
+                             iter_number,
+                             *args, **kwargs)
+def dm_iterate_then_pickle_expXdrift_linearYdrift(file_name, *args, **kwargs):
+    return dm_iterate_then_pickle(expXdrift_linearYdrift_factory,
+                                  file_name,
+                                  *args, **kwargs)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    
     
