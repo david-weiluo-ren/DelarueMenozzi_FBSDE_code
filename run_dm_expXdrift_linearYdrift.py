5feeb55a37f055215ac4b1097552af8157f45d15
buzhihuojie <ren.weiluo@gmail.com>
2016-02-12 10:17:47 -0800

    refactored tradingModel related script

--- run_dm_expXdrift_linearYdrift.py
+++ run_dm_expXdrift_linearYdrift.py
@@ -7,14 +7,11 @@ Created on Jan 29, 2016
 import sys
 from run_dm_iteration_helpers import *
 from dm_expXdrift import dm_iterate_then_pickle_expXdrift_linearYdrift
-
+from run_dm_linearizedXdrift_linearYdrift import generate_tradingModel_parser
 
 def run_dm_expXdrift_linearYdrift():
-    parser = argparse.ArgumentParser()
-    parser = add_dm_sharing_argument_to_parser(parser)
-    parser.add_argument('-A', type=float,nargs='?', help='A in drift of X')
-    parser.add_argument('-kappa', type=float,nargs='?', help='kappa in drift of X')
-    parser.add_argument('-m', type=float,nargs='?', help='terminal condition')
+    parser = generate_tradingModel_parser() 
+    
     arg_dict, file_name = prepare_argdict_and_filename_from_parser(
                             parser, 
                             {"model_type": "expXdrift_linearYdrift"},
--- run_dm_linearizedXdrift_linearYdrift.py
+++ run_dm_linearizedXdrift_linearYdrift.py
@@ -8,13 +8,21 @@ import sys
 from run_dm_iteration_helpers import *
 from dm_linearizedXdrift import dm_iterate_then_pickle_linearizedXdrift_linearYdrift
 
-
-def run_dm_linearizedXdrift_linearYdrift():
-    parser = argparse.ArgumentParser()
-    parser = add_dm_sharing_argument_to_parser(parser)
+def add_tradingModel_argument_to_parser(parser):
     parser.add_argument('-A', type=float,nargs='?', help='A in drift of X')
     parser.add_argument('-kappa', type=float,nargs='?', help='kappa in drift of X')
+    parser.add_argument('-beta', type=float, nargs='?', help='beta in drift of Y')
     parser.add_argument('-m', type=float,nargs='?', help='terminal condition')
+    return parser
+
+def generate_tradingModel_parser():
+    parser = argparse.ArgumentParser()
+    parser = add_dm_sharing_argument_to_parser(parser)
+    parser = add_tradingModel_argument_to_parser(parser)
+    return parser
+
+def run_dm_linearizedXdrift_linearYdrift():
+    parser = generate_tradingModel_parser()
     arg_dict, file_name = prepare_argdict_and_filename_from_parser(
                             parser, 
                             {"model_type": "linearXdrift_linearYdrift"},
